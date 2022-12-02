868210604eb1213941e66d9fbba583b4c47bbbff
Razeb Enyi <enyirazeb@gmail.com>
2022-12-02 10:01:32 +0100

    Update layouts.js

--- components/layouts.js
+++ components/layouts.js
@@ -5,7 +5,6 @@ import Navbar from "./Navbar/Navbar";
 
 export default function Layout({ children }) {
   return <div>
-      {/* <SEO title="Loctech" description="Teaching the next generation" /> */}
       <Banner />
       <Navbar />
       <main className="-mt-36 min-w-screen-full">
